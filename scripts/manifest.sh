3340615ed6211aecd3548d800b11f8138c33bbea
Zheng Weipeng <zheng.weipeng@ctw.inc>
2026-09-20 09:53:57 +0800

    build: bump FFmpeg to 9.0.2

--- scripts/manifest.sh
+++ scripts/manifest.sh
@@ -22,9 +22,9 @@
 # one line here and the matching --enable-* flag in FFMPEG_ENABLE below.
 
 # --- FFmpeg itself ---
-FFMPEG_VERSION="9.0.1"
+FFMPEG_VERSION="9.0.2"
 FFMPEG_URL="https://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.xz"
-FFMPEG_SHA256="cf38e0e28c7e5605942c4a77755349b0145804a397af37eb1fb4c77cb237f635"
+FFMPEG_SHA256="8c3850283eb25fa026482078a04051e0be17347b09ef81a0849bec15a96e002e"
 
 # --- Dependencies (build order matters) ---
 DEPS=(
